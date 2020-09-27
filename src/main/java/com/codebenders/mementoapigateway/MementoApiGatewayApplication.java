6b0a21649563db4a1499d587ebeff536514545cd
tzarmango <fatih.ozceylan@gmail.com>
2020-09-27 20:49:36 +0100

    initial commit

---
+++ src/main/java/com/codebenders/mementoapigateway/MementoApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.codebenders.mementoapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class MementoApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MementoApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codebenders/mementoapigateway/MementoApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebenders.mementoapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MementoApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
